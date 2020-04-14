36d94890582dd33a4e911faebce30a7799dea316
Gowtham422 <63245218+Gowtham422@users.noreply.github.com>
2020-04-14 17:19:10 +0530

    Add files via upload

---
+++ Throws_Declaration.java
@@ -0,0 +1,21 @@
+package exceptions;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Throws_Declaration {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		abc();
+		
+	}
+		
+		public static void abc() throws IOException {
+			FileInputStream x = new FileInputStream("Usersâ©/md/â¨Downloadsâ©/abc.txt");
+
+	}
+
+}
